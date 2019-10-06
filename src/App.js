48d7e3d94ec6f3e6feaa8242425a68bc620aa046
Cameron <cameron.laing@cundari.com>
2019-10-06 14:04:38 -0400

    :tada::fire: Initial emoji commit :fire::tada:

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './common/Header';
 import routes from './routes';
 import { Switch, Route } from 'react-router-dom';
-
+// :)
 const App = () => {
   return (
     <>
